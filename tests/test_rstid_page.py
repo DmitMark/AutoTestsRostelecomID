a32db007c840422b0b937aa0e32eef46c160927f
DmitMark <dmmarkushevski@gmail.com>
2023-10-01 00:24:10 +0300

    refactoring tests

--- tests/test_rstid_page.py
+++ tests/test_rstid_page.py
@@ -5,8 +5,10 @@ from pages.RstID_page import RstIdPage
 def test_open_page(web_browser):
 
     page = RstIdPage(web_browser)
+    page.wait_page_loaded(timeout=10)
 
-    assert page.get_current_url() == 'https://b2c....ount_b2c/page'
+    assert 'https://b2c.passport.rt.ru/auth/realms/b2c/protocol/openid-connect/auth' in page.get_current_url()
+    assert 'Авторизация' in page.get_page_source()
 
 def test_login_valid_email(web_browser):
 
@@ -17,7 +19,8 @@ def test_login_valid_email(web_browser):
     page.password.send_keys('Test123456')
     page.btn_login.click()
 
-    assert page.get_current_url() == 'https://b2c....account_b2c/#/'
+    assert 'https://b2c.passport.rt.ru/account_b2c/page' in page.get_current_url()
+    assert 'Учетные данные' in page.get_page_source()
 
 def test_login_invalid_email(web_browser):
 
@@ -36,11 +39,13 @@ def test_go_page_forgot_pass(web_browser):
     page = RstIdPage(web_browser)
     page.btn_forgot_pass.click()
 
-    assert page.get_current_url() == 'https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/reset-credentials?client_id=account_b2c&tab_id=fDKtZVFrLqI'
+    assert 'https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/reset-credentials' in page.get_current_url()
+    assert 'Восстановление пароля' in page.get_page_source()
 
 def test_go_to_page_registr(web_browser):
 
     page = RstIdPage(web_browser)
     page.btn_register.click()
 
-    assert page.get_current_url() == 'https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/registration?client_id=account_b2c&tab_id=fDKtZVFrLqI'
+    assert 'https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/registration' in page.get_current_url()
+    assert 'Регистрация' in page.get_page_source()
